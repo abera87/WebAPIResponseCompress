d1eaa0f9ff80b758f06db99dae1bcc425a0b8ba7
abera87 <57747453+abera87@users.noreply.github.com>
2020-08-14 11:37:01 +0530

    Initial setup

---
+++ BackOffice/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BackOffice.Model;
+using BackOffice.DBContext;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackOffice.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly BackOfficeDBContext DBContext;
+
+        public EmployeeController(BackOfficeDBContext dBContext)
+        {
+            DBContext = dBContext;
+        }
+        
+        
+        public async Task<ActionResult<IEnumerable<Employee>>> Get()
+        {
+            // var employee = await DBContext.Employee.Include(x=>x.Department).ToListAsync();
+            var employee = await DBContext.Employee.ToListAsync();
+            return Ok(employee);
+        }
+    }
+}
